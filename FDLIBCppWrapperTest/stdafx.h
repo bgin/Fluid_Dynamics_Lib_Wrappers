e56187656bd1901046c7f35869e35ded332d1851
Bernard <beniekg@gmail.com>
2016-04-18 20:29:42 +0200

    Added files via upload
    First commit.

---
+++ FDLIBCppWrapperTest/stdafx.h
@@ -0,0 +1,16 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+// Headers for CppUnitTest
+#include "CppUnitTest.h"
+#include "CppUnitTestLogger.h"
+#include "../FluidDynamicsLibCPPWrapper/lin_alg.h"
+#include "../FluidDynamicsLibCPPWrapper/lin_alg_chol_r.h"
+#include "../FluidDynamicsLibCPPWrapper/StatDistributionWrapper.h"
+// TODO: reference additional headers your program requires here
